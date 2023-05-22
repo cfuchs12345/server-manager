8ec3dfce4f23f0042474ea36d5e60a7a1061b1eb
cfuchs113 <christopher@christopherfuchs.de>
2023-05-22 18:17:13 +0200

    fixed interceptor bug

--- frontend/src/app/auth/error.interceptor.ts
+++ frontend/src/app/auth/error.interceptor.ts
@@ -16,7 +16,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                 // auto logout if 401 response returned from api
                 this.authenticationService.logout();
             }
-            const error = err.error.message || err.statusText;
+            const error = (err.message != null ? err.message : undefined) || err.statusText;
             return throwError(() => new Error(error));
         }))
     }
