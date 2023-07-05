10d7b22008783d8f5d669132d6e60c8276c25901
cfuchs113 <christopher@christopherfuchs.de>
2023-07-05 09:15:45 +0200

    switch to upper case only env vars

--- kafka-agent/src/common.rs
+++ kafka-agent/src/common.rs
@@ -7,13 +7,13 @@ use crate::systeminfo;
 pub const TEN_SECS: std::time::Duration = std::time::Duration::from_secs(10);
 pub const FIVE_SECS: std::time::Duration = std::time::Duration::from_secs(5);
 
-pub const BROKER_ENV_VAR: &str = "broker";
-pub const CONSUME_TOPIC_ENV_VAR: &str = "consume_topic";
-pub const PUBLISH_TOPIC_ENV_VAR: &str = "publish_topic";
-pub const ERROR_TOPIC: &str = "error_topic";
-pub const REGISTRATION_TOPIC: &str = "registration_topic";
-pub const HEARTBEAT_TOPIC: &str = "heartbeat_topic";
-pub const GROUP_VAR: &str = "group";
+pub const BROKER_ENV_VAR: &str = "BROKER";
+pub const CONSUME_TOPIC_ENV_VAR: &str = "CONSUME_TOPIC";
+pub const PUBLISH_TOPIC_ENV_VAR: &str = "PUBLISH_TOPIC";
+pub const ERROR_TOPIC: &str = "ERROR_TOPIC";
+pub const REGISTRATION_TOPIC: &str = "REGISTRATION_TOPIC";
+pub const HEARTBEAT_TOPIC: &str = "HEARTBEAT_TOPIC";
+pub const GROUP_VAR: &str = "GROUP";
 
 lazy_static! {
     pub static ref IS_WINDOWS: bool = cfg!(target_os = "windows");
@@ -25,7 +25,7 @@ pub fn sleep(duration: Duration) {
 }
 
 pub fn get_env_var(name: &str) -> String {
-    match std::env::var(name) {
+    match std::env::var(name.to_uppercase()) {
         Ok(value) => value,
         Err(_) => {
             let message = format!("env var {} not set. Please check that the .env file is present and contains the setting {}", name, name);
